618c3e53681ac92b0530f57b8be66c6622c8e0bd
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-27 12:38:53 +0530

    commit architecture

--- source/neural.py
+++ source/neural.py
@@ -9,6 +9,50 @@
 '''
 
 #1 Loading the dataset (Input and Output)
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+# version of numpy library
+print('Version of numpy:',np.__version__)
+
+# version of matplotlib library
+print('Version of matplotlib:',matplotlib.__version__)
+
+# creating the input array
+X=np.array([[1,0,1,0], [1,0,1,1], [0,1,0,1]])
+print ('\n Input:')
+print(X)
+# shape of input array
+print('\n Shape of Input:', X.shape)
+
+# converting the input in matrix form
+X = X.T
+print('\n Input in matrix form:')
+print(X)
+# shape of input matrix
+print('\n Shape of Input Matrix:', X.shape)
+
+# creating the output array
+y=np.array([[1],[1],[0]])
+print ('\n Actual Output:')
+print(y)
+
+# output in matrix form
+y = y.T
+
+print ('\n Output in matrix form:')
+print(y)
+
+# shape of input array
+print('\n Shape of Output:', y.shape)
+
+## 2. Architecture of the model (# input, hidden and output neurons)
+inputlayer_neurons = X.shape[0] # number of features in data set
+hiddenlayer_neurons = 3 # number of hidden layers neurons
+output_neurons = 1 # number of neurons at output layer
+
 
 
 
