44a254beedab407e5966854f896e8a8cab07aeb0
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-27 12:07:42 +0530

    code file created for nn

---
+++ source/neural.py
@@ -0,0 +1,12 @@
+# Steps to build a Neural Network in NumPy
+'''
+<ol>1. Loading the dataset (Input and Output)</ol>
+<ol>2. Architecture of the model (# input, hidden and output neurons)</ol>
+<ol>3. Initializing the weights for all the layers</ol>
+<ol>4. Implementing forward propagation</ol>
+<ol>5. Implementing backward propagation</ol>
+<ol>6. Train the model for n epochs </ol>
+'''
+
+
+
